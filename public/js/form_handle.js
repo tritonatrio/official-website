d4a474296a3526ff2ec0e98ace46b04f819ef9d0
Oliver Ignetik <olli@ozemail.com.au>
2019-11-14 04:52:03 +1100

    protection from spam

--- public/js/form_handle.js
+++ public/js/form_handle.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#contact').on('click', '.btn-start-order', (e) => {
         e.preventDefault();
-        let email_recipient = "olli@ozemail.com.au"
+        let email_recipient = "olli^ozemail.com.au".replace('^','@');
         // mail to to
         window.open("mailto:" + email_recipient + "?subject=" + $('#subject').value + "&body=" + $('#message').value)+"&name="+$('#name').value+"&mailfrom="+$('#email').value;
     });
