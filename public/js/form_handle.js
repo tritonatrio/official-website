95aa178586cb42623b8cbab591043fb6a239c08e
Oliver Ignetik <olli@ozemail.com.au>
2019-11-14 05:18:46 +1100

    mail test

--- public/js/form_handle.js
+++ public/js/form_handle.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $('.btn-start-order').on('click', (e) => {
         // extract data
-        let subject = $('#subject').value;
-        let body = $('#message').value;
-        console.log(body);
+        let subject = $('#subject')[0].value;
+        let body = $('#message')[0].value;
+        let email = $('#email')[0].value;
         e.preventDefault();
         let email_recipient = "olli^ozemail.com.au".replace('^','@');
         // mail to
-        window.open("mailto:" + email_recipient + "?subject=" + $('#subject').value + "&body=" + $('#message').value);
+        window.open("mailto:" + email_recipient + "?subject=" + subject + "&body=" + body+'&from='+email);
     });
 
 });
