11df03c9f8df25774375f6e538001664ad3342db
Oliver Ignetik <olli@ozemail.com.au>
2019-11-14 05:27:34 +1100

    mail test

--- public/js/form_handle.js
+++ public/js/form_handle.js
@@ -1,12 +1,11 @@
 $(document).ready(function () {
     $('.btn-start-order').on('click', (e) => {
         // extract data
-        let subject = $('#subject')[0].value;
-        let body = $('#message')[0].value;
-        let email = $('#email')[0].value;
-        e.preventDefault();
+        let subject = JSON.stringify($('#subject')[0].value);
+        let body = JSON.stringify($('#message')[0].value);
+        // let email = JSON.Stringify($('#email')[0].value);
         let email_recipient = "olli^ozemail.com.au".replace('^','@');
-        // mail to
+        // mailto:olli@ozemail.com.au?subject=trio_info&body=My%20name%20is%20Olli
         window.open("mailto:" + email_recipient + "?subject=" + subject + "&body=" + body);
     });
 
