04c241ab5e90f4d565a6b35a5383fb657b34c31b
Oliver Ignetik <olli@ozemail.com.au>
2019-11-16 12:11:14 +1100

    16-11-19 mail bug

--- public/js/form_handle.js
+++ public/js/form_handle.js
@@ -5,6 +5,6 @@ function sendMail() {
     let subject = $('#subject')[0].value;
     let body = $('#message')[0].value;
     location.href = 'mailto:' + email_recipient
-        + '?subject=' + encodeURIComponent(subject)
-        + '&body=' + encodeURIComponent(body)
+        + '?subject=' + subject
+        + '&body=' + body
 }
