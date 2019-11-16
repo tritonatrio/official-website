7803f57a31ee514ec4dc899fa389c521a80a5056
Oliver Ignetik <olli@ozemail.com.au>
2019-11-16 12:04:02 +1100

    16-11-19

--- public/js/form_handle.js
+++ public/js/form_handle.js
@@ -1,15 +1,10 @@
 // mail handler
 function sendMail() {
     // to make sure spammers leave you alone
-    let email_recipient = "Tr%ton$Tr%o^gm$%l.com"
-    .replace('^','@')
-    .replace('%','i')
-    .replace('$','a');
-    let email = $('#email')[0].value;
+    let email_recipient = "TritonaTrio^gmail.com".replace('^','@');
     let subject = $('#subject')[0].value;
     let body = $('#message')[0].value;
     location.href = 'mailto:' + email_recipient
         + '?subject=' + encodeURIComponent(subject)
         + '&body=' + encodeURIComponent(body)
-        + '$cc='+encodeURIComponent(email);
 }
