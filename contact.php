5b8196acffa769ff99688061b7fb05fc36ac4059
Oliver Ignetik <olli@ozemail.com.au>
2019-11-16 21:09:55 +1100

    16-11-19 added php submit

--- contact.php
+++ contact.php
@@ -6,10 +6,12 @@ if (isset($_POST['submit'])) {
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $mailTo = "TritonaTrio^gmail.com".replace('^','@');
+    $mailTo = str_replace('^','@','TritonaTrio^gmail.com');
     $headers = "From: ".$mailFrom;
     $txt = "Email from: ".$name."\n\n Tritona website\n\n".$message;
 
     mail($mailTo, $subject,$txt,$headers) or die("There was an error please try again!"); 
-    echo "Thankyou for getting in touch!"
+    echo "Thankyou for getting in touch!";
 }
+
+?>
