58717d12a96ed383cdc8bc4d73d4d91354315a37
Oliver Ignetik <olli@ozemail.com.au>
2020-02-24 23:10:50 +1100

    changed instagram feed code and thumbnail

--- public/js/gramline.js
+++ public/js/gramline.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-  let userFeed = new Instafeed({
-    get: 'user',
-    userId: '23211116930',
-    limit: 10,
-    resolution: 'standard_resolution',
-    accessToken: '23211116930.1677ed0.44e1039a4c6f475aafb4192898a813ac',
-    sortBy: 'most-recent',
-    filter: function(image) {
-      return image.type === 'image';
-    },
-    template: `<div class="col-4 gallery instaimg">
-            <a href="{{link}}" title="{{caption}}" target="{{_blank}}">
-                <img src="{{image}}" alt="{{caption}}" class="img-fluid"/>
-            </a>
-         </div>`,
-  });
-  userFeed.run();
+  (function() {
+    new InstagramFeed({
+      username: 'tritonatrio',
+      container: document.getElementById('instafeed'),
+      display_profile: false,
+      display_biography: false,
+      display_gallery: true,
+      callback: null,
+      styling: true,
+      items: 9,
+      items_per_row: 3,
+      margin: 1,
+    });
+  })();
 });
