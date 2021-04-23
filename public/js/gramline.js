0f4c9329a270a4a6591829083e0862be3a6e4024
Oliver Ignetik <olli@ozemail.com.au>
2021-04-23 11:03:31 +1000

    removed instafeed due to CORS error 23-04-21

--- public/js/gramline.js
+++ public/js/gramline.js
@@ -1,16 +1,17 @@
-$(document).ready(function() {
-  (function() {
-    new InstagramFeed({
-      username: 'tritonatrio',
-      container: document.getElementById('instafeed'),
-      display_profile: false,
-      display_biography: false,
-      display_gallery: true,
-      callback: null,
-      styling: true,
-      items: 9,
-      items_per_row: 3,
-      margin: 1,
+(function($){
+  $(window).on('load', function(){
+    $.instagramFeed({
+      'tag': 'tritonatrio',
+      'container': document.getElementById('instafeed'),
+      'display_profile': false,
+      'display_biography': false,
+      'display_gallery': true,
+      'callback': null,
+      'styling': true,
+      'items': 9,
+      'items_per_row': 3,
+      'margin': 1,
+      'on_error' : console.error
     });
-  })();
-});
+  });
+})(jQuery);
